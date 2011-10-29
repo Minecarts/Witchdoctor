5a7efd6ad25afea759316751511bd4c53bbf3c1f
Stephen <stephen@jazer.com>
2011-10-28 20:40:28 -0400

    Updated pom

--- src/com/minecarts/witchdoctor/listener/PlayerListener.java
+++ src/com/minecarts/witchdoctor/listener/PlayerListener.java
@@ -2,18 +2,21 @@ package com.minecarts.witchdoctor.listener;
 
 import com.minecarts.witchdoctor.EffectTrackerEntry;
 import com.minecarts.witchdoctor.Witchdoctor;
+import net.minecraft.server.MobEffect;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.sql.Timestamp;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
 public class PlayerListener extends org.bukkit.event.player.PlayerListener {
     private Witchdoctor plugin;
+    private ArrayList<MobEffectTrackingEntry> reapplyEffectList = new ArrayList<MobEffectTrackingEntry>();
 
     public PlayerListener(Witchdoctor plugin){
         this.plugin = plugin;
@@ -31,10 +34,29 @@ public class PlayerListener extends org.bukkit.event.player.PlayerListener {
                 //System.out.println("Tracked effect founnd, remaining time: " + remainingTime);
                 if(remainingTime > 0){
                     Bukkit.getScheduler().cancelTask(entry.taskId); //Cancel the old pending task
-                    plugin.applyPotion(p,entry.mobEffect.getEffectId(),remainingTime,entry.mobEffect.getAmplifier());
+                    reapplyEffectList.add(new MobEffectTrackingEntry(p,entry.mobEffect,remainingTime));
                 }
             }
+
+            //Clear the (old) tracked effects
             plugin.clearTrackedEffects(p.getName());
+
+            //Reapply the potion effects (with the updated remaining time
+            for(MobEffectTrackingEntry entry : reapplyEffectList){
+                plugin.applyPotion(entry.player,entry.mobEffect.getEffectId(),entry.remainingTime,entry.mobEffect.getAmplifier());
+            }
+        }
+    }
+
+    private class MobEffectTrackingEntry{
+        public Player player;
+        public MobEffect mobEffect;
+        public int remainingTime;
+
+        public MobEffectTrackingEntry(Player p, MobEffect me, int time){
+            this.player = p;
+            this.mobEffect = me;
+            this.remainingTime = time;
         }
     }
 }
